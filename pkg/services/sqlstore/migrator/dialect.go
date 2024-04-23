9179480587cdc9c0ad781dfd27cb9e2b4ea894c4
huachenglong <huachenglong@canway.net>
2024-04-23 16:00:02 +0800

    feat: 达梦适配

--- pkg/services/sqlstore/migrator/dialect.go
+++ pkg/services/sqlstore/migrator/dialect.go
@@ -61,6 +61,7 @@ var supportedDialects = map[string]dialectFunc{
 	MySQL:                  NewMysqlDialect,
 	SQLite:                 NewSQLite3Dialect,
 	Postgres:               NewPostgresDialect,
+	DM:                     NewPostgresDialect,
 	MySQL + "WithHooks":    NewMysqlDialect,
 	SQLite + "WithHooks":   NewSQLite3Dialect,
 	Postgres + "WithHooks": NewPostgresDialect,
