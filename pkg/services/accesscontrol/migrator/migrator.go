8b94a84f5425fcb2fcdc57b2c51444af81d96c24
RyanYu <524860467@qq.com>
2025-09-04 21:26:46 +0800

    fix: 达梦permision表更新问题

--- pkg/services/accesscontrol/migrator/migrator.go
+++ pkg/services/accesscontrol/migrator/migrator.go
@@ -77,6 +77,10 @@ func MigrateScopeSplit(db db.DB, log log.Logger) error {
 
 		// Batch update the permissions
 		if errBatchUpdate := db.GetSqlxSession().WithTransaction(ctx, func(tx *session.SessionTx) error {
+			if _, errDel := tx.Exec(ctx, `SET IDENTITY_INSERT "permission" ON`); errDel != nil {
+				log.Error("Error SET IDENTITY_INSERT permission ON", "migration", "scopeSplit", "error", errDel)
+				return errDel
+			}
 			if _, errDel := tx.Exec(ctx, delQuery, delArgs...); errDel != nil {
 				log.Error("Error deleting permissions", "migration", "scopeSplit", "error", errDel)
 				return errDel
